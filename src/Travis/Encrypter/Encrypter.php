a320e6564d0462cd610a5cf1b409fad8b307c338
peter279k <peter279k@gmail.com>
2017-11-01 09:01:51 +0800

    replace sign with encrypt method

--- src/Travis/Encrypter/Encrypter.php
+++ src/Travis/Encrypter/Encrypter.php
@@ -43,7 +43,7 @@ class Encrypter
         //openssl_public_encrypt($this->envName.'='.$this->envValue, $result, $this->key);
         $rsa = new RSA();
         $rsa->loadKey($this->key);
-        $result = $rsa->encrypt($this->envName.'='.$this->envValue, 6);
+        $result = $rsa->sign($this->envName.'='.$this->envValue, 2);
 
         return base64_encode($result);
     }
